00b3a51abcc03e629bb80881107c2bd1663b47f5
Steven Cao <steven.cao@chekrite.com>
2019-08-24 11:06:28 +1000

    Install and setup webpack, babel, jest

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.bundle.js"
+  },
+  mode: "production",
+  module: {
+    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }]
+  }
+};
